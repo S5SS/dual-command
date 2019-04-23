3d74ebaf1a9c109fbed8dd238e5ea9ed877e2832
Novaras <thomas_fear@yahoo.co.uk>
2019-04-23 01:15:54 +0100

    hgn refresh 1 => 0.5

--- Data/scripts/ult_underattackreissuedock.lua
+++ Data/scripts/ult_underattackreissuedock.lua
@@ -1,7 +1,11 @@
 function Ult_UnderAttackReissueDock(group)
-    if (SobGroup_GetCurrentOrder(group) == COMMAND_Dock) then
-        if (SobGroup_UnderAttack(group)) then
-            SobGroup_DockSobGroupWithAny(group)
+    if (SobGroup_GetCurrentOrder(group) == COMMAND_Dock) then -- docking
+        if (SobGroup_UnderAttack(group)) then -- under attack
+            if (SobGroup_Count(group) < SobGroup_GetStaticF(group, "buildBatch")) then -- lost one or more members
+                if (SobGroup_GetActualSpeed(group) < 50) then -- probably bugged into stopping - could get unlucky here and catch a pivoting squad
+                    SobGroup_DockSobGroupWithAny(group)
+                end
+            end
         end
     end
 end
