50e85d8a294736fc28f6eee71e7e88886d0b0dd7
S5SS <plzplzspammeplzplz@gmail.com>
2019-04-25 04:20:23 +0100

    changed mod name to 2.4 players patch

--- Data/build.lua
+++ Data/build.lua
@@ -1,3 +1,2 @@
-modName = "Dual Command Mode" -- Official name
-modNameShort = "Dual Command" -- Shortened name for things like gamemode names (not used atm)
+modName = "v2.4 Players Patch"
 modBuild = 10
