8fb17bd1df844916124a8f27f7ac60a2e129a830
Novaras <thomas_fear@yahoo.co.uk>
2019-05-06 07:07:02 +0100

    drone frigates actually shoot

--- Data/ship/kus_dronefrigate/kus_dronefrigate.lua
+++ Data/ship/kus_dronefrigate/kus_dronefrigate.lua
@@ -1,4 +1,13 @@
-drone_attacking = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 }
+function SobGroup_AnyBeingCaptured(group)
+	local group_being_captured = group .. "_being_captured"
+	SobGroup_CreateIfNotExist(group_being_captured)
+	SobGroup_Clear(group_being_captured)
+	SobGroup_GetSobGroupBeingCapturedGroup(group, group_being_captured)
+	if (SobGroup_Count(group_being_captured) > 0) then
+		return 1
+	end
+	return 0
+end
 
 function Start_DroneFrigate(CustomGroup, playerIndex, shipID) 
 	SobGroup_SetSpeed(CustomGroup, 0.9)
@@ -8,10 +17,7 @@ end
 
 function Do_DroneFrigate(CustomGroup, playerIndex, shipID)  	
 	SobGroup_CreateIfNotExist("dronefrigate_temp")
-	SobGroup_CreateIfNotExist("dronefrigate_temp1")
-	SobGroup_CreateIfNotExist("dronefrigate_temp2")				
-	SobGroup_Clear("dronefrigate_temp2")		
-	SobGroup_GetSobGroupBeingCapturedGroup(CustomGroup, "dronefrigate_temp2")		
+	SobGroup_CreateIfNotExist("dronefrigate_temp1")	
 	for k = 0,13,1 do
 		SobGroup_CreateIfNotExist("kus_drone" .. tostring(shipID) .. tostring(k))		
 		SobGroup_Clear("dronefrigate_temp")
@@ -25,7 +31,7 @@ function Do_DroneFrigate(CustomGroup, playerIndex, shipID)
 			SobGroup_AreAllInRealSpace(CustomGroup) == 1 and
 			SobGroup_IsDoingAbility(CustomGroup, AB_Dock) == 0 and				
 			SobGroup_IsDoingAbility(CustomGroup, AB_Retire) == 0 and
-			SobGroup_Count("dronefrigate_temp2") == 0 then
+			SobGroup_AnyBeingCaptured(CustomGroup) == 0 then
 			local CreateGroup = SobGroup_CreateShip(CustomGroup, "kus_drone"..k)
 			SobGroup_SobGroupAdd("kus_drone" .. tostring(shipID) .. tostring(k), CreateGroup)
 			if k == 0 or k == 4 or k == 8 or k == 12 then
@@ -46,7 +52,7 @@ function Do_DroneFrigate(CustomGroup, playerIndex, shipID)
 			SobGroup_AreAllInRealSpace(CustomGroup) == 1 and
 			SobGroup_IsDoingAbility(CustomGroup, AB_Dock) == 0 and				
 			SobGroup_IsDoingAbility(CustomGroup, AB_Retire) == 0 and
-			SobGroup_Count("dronefrigate_temp2") == 0 then
+			SobGroup_AnyBeingCaptured(CustomGroup) == 0 then
 				SobGroup_Launch("kus_drone" .. tostring(shipID) .. tostring(k), CustomGroup)
 				if k == 0 or k == 4 or k == 8 or k == 12 then
 					FX_StartEvent(CustomGroup, "dronelaunch1")
@@ -82,17 +88,17 @@ function Create_DroneFrigate(CustomGroup, playerIndex, shipID)
 	SobGroup_SetSwitchOwnerFlag(CustomGroup, 0)
 	SobGroup_CreateIfNotExist("dronefrigate_temp")
 	SobGroup_CreateIfNotExist("dronefrigate_temp1")
-	SobGroup_CreateIfNotExist("dronefrigate_temp2")
+
+	SobGroup_CreateIfNotExist("attack_targets" .. shipID)
 	for k = 0,13,1 do
-		SobGroup_CreateIfNotExist("kus_drone" .. tostring(shipID) .. tostring(k))		
+		local new_drone = "kus_drone" .. tostring(shipID) .. tostring(k)
+		SobGroup_CreateIfNotExist(new_drone)	
 	end
 end
 
 function Update_DroneFrigate(CustomGroup, playerIndex, shipID)
 	SobGroup_AbilityActivate(CustomGroup, AB_Scuttle, 1 - SobGroup_IsDoingAbility(CustomGroup, AB_Dock))
-	SobGroup_CreateIfNotExist("dronefrigate_temp2")
-	SobGroup_Clear("dronefrigate_temp2")	
-	SobGroup_GetSobGroupBeingCapturedGroup(CustomGroup, "dronefrigate_temp2")
+
 	if Player_GetLevelOfDifficulty(playerIndex) > 0 then
 		local playerIndex_ai = 0		
 		local enemyships = 0
@@ -113,45 +119,49 @@ function Update_DroneFrigate(CustomGroup, playerIndex, shipID)
 			end
 		end
 	end
-	for k = 0,13,1 do
-		if SobGroup_Empty("kus_drone" .. tostring(shipID) .. tostring(k)) == 0 then
-			if SobGroup_IsDockedSobGroup("kus_drone" .. tostring(shipID) .. tostring(k), CustomGroup) == 0 and
-			SobGroup_IsDoingAbility("kus_drone" .. tostring(shipID) .. tostring(k), AB_Parade) == 0 and
-			SobGroup_IsDoingAbility("kus_drone" .. tostring(shipID) .. tostring(k), AB_Dock) == 0 then
-				SobGroup_ParadeSobGroup("kus_drone" .. tostring(shipID) .. tostring(k), CustomGroup, 0)
+	for k = 0, 13, 1 do
+		local this_drone = "kus_drone" .. tostring(shipID) .. tostring(k)
+		print("updating drone " .. k)
+		print("\t" ..  this_drone)
+		SobGroup_SetROE(this_drone, SobGroup_GetROE(CustomGroup))
+		if SobGroup_Empty(this_drone) == 0 then
+			if SobGroup_IsDockedSobGroup(this_drone, CustomGroup) == 0 and
+			SobGroup_IsDoingAbility(this_drone, AB_Parade) == 0 and
+			SobGroup_IsDoingAbility(this_drone, AB_Dock) == 0 then
+				if (SobGroup_GetDistanceToSobGroup(this_drone, CustomGroup) < 150 or
+				SobGroup_GetDistanceToSobGroup(this_drone, CustomGroup) > 230) then
+					SobGroup_ParadeSobGroup(this_drone, CustomGroup, 0) -- reform parade around frigate
+				end
 			end
-			if SobGroup_GetDistanceToSobGroup("kus_drone" .. tostring(shipID) .. tostring(k), CustomGroup) > 950 then
-				SobGroup_TakeDamage("kus_drone" .. tostring(shipID) .. tostring(k), 1)
-			elseif SobGroup_GetDistanceToSobGroup("kus_drone" .. tostring(shipID) .. tostring(k), CustomGroup) > 275 then
-				SobGroup_AbilityActivate("kus_drone" .. tostring(shipID) .. tostring(k), AB_Targeting, 0)
-				SobGroup_AbilityActivate("kus_drone" .. tostring(shipID) .. tostring(k), AB_Attack, 0)
+			if SobGroup_GetDistanceToSobGroup(this_drone, CustomGroup) > 950 then -- too far from frigate, die
+				SobGroup_TakeDamage(this_drone, 1)
+			elseif SobGroup_GetDistanceToSobGroup(this_drone, CustomGroup) > 275 then -- too far from frigate, no attack
+				SobGroup_AbilityActivate(this_drone, AB_Targeting, 0)
+				SobGroup_AbilityActivate(this_drone, AB_Attack, 0)
 			else
-				SobGroup_AbilityActivate("kus_drone" .. tostring(shipID) .. tostring(k), AB_Targeting, 1)
-				SobGroup_AbilityActivate("kus_drone" .. tostring(shipID) .. tostring(k), AB_Attack, 1)
-				SobGroup_GuardSobGroup ("kus_drone" .. tostring(shipID) .. tostring(k), CustomGroup) 
+				SobGroup_AbilityActivate(this_drone, AB_Targeting, 1) -- distance ok, allow targeting and attacking, begin guard of frigate
+				SobGroup_AbilityActivate(this_drone, AB_Attack, 1)
+				if (SobGroup_IsDoingAbility(this_drone, AB_Guard) == 0) then
+					SobGroup_GuardSobGroup (this_drone, CustomGroup)
+				end
 			end			
 			if SobGroup_IsDoingAbility(CustomGroup, AB_Hyperspace) == 1 or
 			SobGroup_IsDoingAbility(CustomGroup, AB_HyperspaceViaGate) == 1 or
 			SobGroup_AreAllInRealSpace(CustomGroup) == 0 or
 			SobGroup_IsDoingAbility(CustomGroup, AB_Dock) == 1 or			
 			SobGroup_IsDoingAbility(CustomGroup, AB_Retire) == 1 or
-			SobGroup_Count("dronefrigate_temp2") > 0 or
+			SobGroup_AnyBeingCaptured(CustomGroup) > 0 or
 			SobGroup_OwnedBy("kus_drone" .. tostring(shipID) .. tostring(k)) ~= playerIndex then					
 				SobGroup_TakeDamage("kus_drone" .. tostring(shipID) .. tostring(k), 1)
 				--SobGroup_DockSobGroupInstant("kus_drone" .. tostring(shipID) .. tostring(k), CustomGroup)
 			end
+
 			if SobGroup_IsDoingAbility(CustomGroup, AB_Attack) == 1 then
-				if drone_attacking[k] == -1 or drone_attacking[k] == DefensiveROE then
-					SobGroup_SetROE("kus_drone" .. tostring(shipID) .. tostring(k), OffensiveROE) 
-					drone_attacking[k] = OffensiveROE
-				end
-			else
-			if SobGroup_IsDoingAbility(CustomGroup, AB_Attack) == 0 then
-				if drone_attacking[k] == -1 or drone_attacking[k] == OffensiveROE then
-					SobGroup_SetROE("kus_drone" .. tostring(shipID) .. tostring(k), DefensiveROE) 
-					drone_attacking[k] = DefensiveROE
-				end
-			end
+				SobGroup_GetCommandTargets("attack_targets" .. shipID, CustomGroup, COMMAND_Attack)
+				SobGroup_Attack(playerIndex, this_drone, "attack_targets" .. shipID)
+			elseif SobGroup_IsDoingAbility(CustomGroup, AB_Attack) == 0 then
+					SobGroup_AbilityActivate(this_drone, AB_Attack, 0)
+					SobGroup_AbilityActivate(this_drone, AB_Targeting, 0)
 			end
 		end	
 	end	
